e85a0a09fa93a04755eaf59cf87e2dabcf0412fb
omar6685 <omr6685@hotmail.com>
2024-10-18 20:43:33 +0300

    rout & controller

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use('/openai', require('./routes/openaiRoutes.js'))
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
